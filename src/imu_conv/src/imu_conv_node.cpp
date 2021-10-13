26004f753c687c878086591f02300a6c3257baa2
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2021-10-13 19:20:20 +0200

    Update imu_conv and twist_conv

--- src/imu_conv/src/imu_conv_node.cpp
+++ src/imu_conv/src/imu_conv_node.cpp
@@ -1,12 +1,42 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 #include "tf/tf.h"
+#include "Eigen/Dense"
 
 sensor_msgs::Imu imu_sub_msg;
 
 void imu_sub_cb(const sensor_msgs::Imu::ConstPtr& imu)
 {
 	imu_sub_msg = *imu;
+}
+
+// Inspired from https://github.com/cra-ros-pkg/robot_localization/blob/32896d6d1aaec5a92c3a65e5f16dfc0b859a7d26/src/ros_filter.cpp#L1556 and 
+// https://github.com/cra-ros-pkg/robot_localization/blob/32896d6d1aaec5a92c3a65e5f16dfc0b859a7d26/params/ukf_template.yaml#L209
+// Should check more parsing step...?
+Eigen::MatrixXd matrixparam(ros::NodeHandle nh, const std::string param, Eigen::MatrixXd matrixdefault)
+{
+	Eigen::MatrixXd matrix(matrixdefault);
+	XmlRpc::XmlRpcValue matrix_tmp;
+
+	nh.getParam(param, matrix_tmp);
+
+	if (matrix_tmp.getType() != XmlRpc::XmlRpcValue::TypeArray) return matrixdefault;
+
+	for (int i = 0; i < matrix.rows(); i++)
+	{
+		for (int j = 0; j < matrix.cols(); j++)
+		{
+			// These matrices can cause problems if all the types
+			// aren't specified with decimal points. Handle that
+			// using string streams.
+			std::ostringstream ostr;
+			ostr << matrix_tmp[matrix.rows()*i+j];
+			std::istringstream istr(ostr.str());
+			istr >> matrix(i, j);
+		}
+	}
+
+	return matrix;
 }
 
 int main(int argc, char** argv) {
@@ -16,23 +46,36 @@ int main(int argc, char** argv) {
 	ros::Subscriber sub = nh.subscribe<sensor_msgs::Imu>(nh.param<std::string>("subscriber/topic", "/imu"), nh.param("subscriber/queue", 1000), imu_sub_cb);
 	ros::Publisher pub = nh.advertise<sensor_msgs::Imu>(nh.param<std::string>("publisher/topic", "/imu/data"), nh.param("publisher/queue", 1000));
 
+	Eigen::MatrixXd angular_velocity_transform_matrix = matrixparam(nh, "angular_velocity_transform_matrix", Eigen::MatrixXd(4, 4));
+	Eigen::MatrixXd linear_acceleration_transform_matrix = matrixparam(nh, "linear_acceleration_transform_matrix", Eigen::MatrixXd(4, 4));
+	Eigen::MatrixXd RPY_transform_matrix = matrixparam(nh, "RPY_transform_matrix", Eigen::MatrixXd(4, 4));
+
 	ros::Rate rate(nh.param("rate", 50));
 
 	while (ros::ok()) {
 		sensor_msgs::Imu imu_pub_msg;
 
 		imu_pub_msg = imu_sub_msg;
-		imu_pub_msg.angular_velocity.x = nh.param("angular_velocity_x_coef", 1.0)*imu_sub_msg.angular_velocity.x;
-		imu_pub_msg.angular_velocity.y = nh.param("angular_velocity_y_coef", 1.0)*imu_sub_msg.angular_velocity.y;
-		imu_pub_msg.angular_velocity.z = nh.param("angular_velocity_z_coef", 1.0)*imu_sub_msg.angular_velocity.z;
-		imu_pub_msg.linear_acceleration.x = nh.param("linear_acceleration_x_coef", 1.0)*imu_sub_msg.linear_acceleration.x;
-		imu_pub_msg.linear_acceleration.y = nh.param("linear_acceleration_y_coef", 1.0)*imu_sub_msg.linear_acceleration.y;
-		imu_pub_msg.linear_acceleration.z = nh.param("linear_acceleration_z_coef", 1.0)*imu_sub_msg.linear_acceleration.z;
+
+		Eigen::Vector4d angular_velocity_sub(imu_sub_msg.angular_velocity.x, imu_sub_msg.angular_velocity.y, imu_sub_msg.angular_velocity.z, 1.0);
+		Eigen::Vector4d angular_velocity_pub = angular_velocity_transform_matrix*angular_velocity_sub;
+		imu_pub_msg.angular_velocity.x = angular_velocity_pub(0);
+		imu_pub_msg.angular_velocity.y = angular_velocity_pub(1);
+		imu_pub_msg.angular_velocity.z = angular_velocity_pub(2);
+
+		Eigen::Vector4d linear_acceleration_sub(imu_sub_msg.linear_acceleration.x, imu_sub_msg.linear_acceleration.y, imu_sub_msg.linear_acceleration.z, 1.0);
+		Eigen::Vector4d linear_acceleration_pub = linear_acceleration_transform_matrix*linear_acceleration_sub;
+		imu_pub_msg.linear_acceleration.x = linear_acceleration_pub(0);
+		imu_pub_msg.linear_acceleration.y = linear_acceleration_pub(1);
+		imu_pub_msg.linear_acceleration.z = linear_acceleration_pub(2);
+
 		tf::Quaternion q;
 		double roll = 0, pitch = 0, yaw = 0;
 		tf::quaternionMsgToTF(imu_sub_msg.orientation, q);
 		tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
-		q.setRPY(nh.param("roll_coef", 1.0)*roll, nh.param("pitch_coef", 1.0)*pitch, nh.param("yaw_coef", 1.0)*yaw);
+		Eigen::Vector4d RPY_sub(roll, pitch, yaw, 1.0);
+		Eigen::Vector4d RPY_pub = RPY_transform_matrix*RPY_sub;
+		q.setRPY(RPY_pub(0), RPY_pub(1), RPY_pub(2));
 		imu_pub_msg.orientation.x = q[0];
 		imu_pub_msg.orientation.y = q[1];
 		imu_pub_msg.orientation.z = q[2];
